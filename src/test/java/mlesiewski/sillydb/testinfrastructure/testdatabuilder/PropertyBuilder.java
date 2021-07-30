2af4afb9d2cb23bacb0726576129577f40509da2
tirpitz-verus <tirpitz-verus@gmail.com>
2021-07-30 23:56:27 +0200

    0.9 BigDecimalPropertyValue
    * introduce BigDecimalPropertyValue along with tests and test infrastructure
    * update roadmap in README
    * bump varsion to 0.9.0

--- src/test/java/mlesiewski/sillydb/testinfrastructure/testdatabuilder/PropertyBuilder.java
+++ src/test/java/mlesiewski/sillydb/testinfrastructure/testdatabuilder/PropertyBuilder.java
@@ -3,12 +3,17 @@ package mlesiewski.sillydb.testinfrastructure.testdatabuilder;
 import mlesiewski.sillydb.*;
 import mlesiewski.sillydb.propertyvalue.*;
 
+import java.math.*;
+
 public interface PropertyBuilder extends GenericBuilder {
 
     PropertyBuilder withProperty(PropertyName name, String value);
 
     PropertyBuilder withProperty(PropertyName name, boolean value);
 
+    PropertyBuilder withProperty(PropertyName name, BigDecimal value);
+
     <T> PropertyBuilder withProperty(PropertyName name, PropertyValue<T> value);
+
     ThingWithCategory getThing();
 }
